7116ca31c9273c0f565aaa7b3e51a76b2d50ac38
Darshan Shaligram <scintilla@gmail.com>
2012-11-09 16:36:33 -0500

    Fix regex to handle double-digit ABV.

--- lib/analyst-lib/metadata.rb
+++ lib/analyst-lib/metadata.rb
@@ -10,7 +10,7 @@ class AnalystLib::Metadata
     content = metadata.search('tr')
     @rating_score = content.search('//span[@class="BAscore_big"]').first.text
     @rating_desc = "CANNOT FIND"
-    @abv = content.search('td')[1].text.match(/\d{1}.\d{2}% ABV/)[0].split('%')[0]
+    @abv = content.search('td')[1].text.match(/\d+(?:[.]\d+)?% ABV/)[0].split('%')[0]
     @description = "CANNOT FIND"
   end
 end
