1a83f41fbdec863718aaa7ae352598a525a0f9ce
Darshan Shaligram <scintilla@gmail.com>
2012-11-14 22:53:48 -0500

    Correctly force encoding to the known encoding where we know the encoding and the site doesn't bother declaring it.

--- lib/analyst-lib/scraper.rb
+++ lib/analyst-lib/scraper.rb
@@ -4,6 +4,6 @@ class AnalystLib::Scraper
   def self.scrape_url(url, content_encoding=nil)
     text = open(url).read()
     text = text.force_encoding(content_encoding) if content_encoding
-    Nokogiri::HTML(AnalystLib::TextEncoding.encode(open(url).read()))
+    Nokogiri::HTML(AnalystLib::TextEncoding.encode(text))
   end
 end
